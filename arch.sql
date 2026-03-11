39d0aebffc5ef9d75ab7dcb2112b479e169682d8
Axawys <52900829+Axawys@users.noreply.github.com>
2026-03-11 10:00:42 +0300

    Update arch.sql

--- arch.sql
+++ arch.sql
@@ -1,89 +1,53 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Хост: 127.0.0.1:3306
--- Время создания: Окт 07 2024 г., 11:05
--- Версия сервера: 5.7.39
--- Версия PHP: 7.3.33
+-- Adminer 5.4.2 MySQL 8.4.8 dump
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+SET NAMES utf8mb4;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- База данных: `candies`
---
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `products`
---
-
+DROP TABLE IF EXISTS `employees`;
 CREATE TABLE `employees` (
-  `id` int(11) UNSIGNED NOT NULL,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
   `img_path` varchar(45) NOT NULL,
   `name` varchar(45) NOT NULL,
-  `birth` int(11) NOT NULL,
-  `id_type` int(11) NOT NULL,
-  `description` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Дамп данных таблицы `products`
---
+  `birth` int NOT NULL,
+  `id_type` int NOT NULL,
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 INSERT INTO `employees` (`id`, `img_path`, `name`, `birth`, `id_type`, `description`) VALUES
-(2, '2.jpg', 'Виктор Александрович Кузнецов', 1950, 4, 'Опытный лидер с острым умом и прямолинейным характером. Его обширный опыт направляет фирму с мудростью и чувством традиции.'),
-(5, '5.jpg', 'Алексей Павлович Морозов', 1980, 3, 'Внимательный к деталям новатор с любовью к современным архитектурным эстетикам и преданностью удовлетворению клиентов.'),
-(7, '7.jpg', 'Николай Евгеньевич Волков', 1975, 2, 'Надежный и аналитический мыслитель, известный своей экспертизой в обеспечении структурной целостности каждого проекта.'),
-(1, '1.jpg', 'Елена Васильевна Петрова', 1985, 1, 'Скрупулезная и творческая личность с passion к устойчивому дизайну. Известна своей способностью руководить сложными проектами с спокойствием и вдохновляющим подходом.'),
-(3, '3.jpg', 'Григорий Иванович Соколов', 1948, 7, 'Харизматичная и авторитетная фигура, уважаемая за стратегическое видение и глубокое понимание градостроительства.'),
-(4, '4.jpg', 'Дмитрий Сергеевич Иванов', 1978, 8, 'Энергичный и общительный командный игрок, мастерски координирующий проекты и способствующий сотрудничеству в коллективе.'),
-(6, '6.jpg', 'Софья Михайловна Лебедева', 1992, 1, 'Яркая и энтузиастка-новичок, жаждущая учиться и привносить свежие идеи с помощью своих смелых концепций дизайна.'),
-(8, '8.jpg', 'Олег Анатольевич Смирнов', 1965, 6, 'Прагматичный и организованный лидер, который обеспечивает бесперебойную работу фирмы своим эффективным стилем управления.');
+(1,	'1.jpg',	'Елена Васильевна Петрова',	1985,	1,	'Скрупулезная и творческая личность с passion к устойчивому дизайну. Известна своей способностью руководить сложными проектами с спокойствием и вдохновляющим подходом.'),
+(2,	'2.jpg',	'Виктор Александрович Кузнецов',	1950,	4,	'Опытный лидер с острым умом и прямолинейным характером. Его обширный опыт направляет фирму с мудростью и чувством традиции.'),
+(3,	'3.jpg',	'Григорий Иванович Соколов',	1948,	7,	'Харизматичная и авторитетная фигура, уважаемая за стратегическое видение и глубокое понимание градостроительства.'),
+(4,	'4.jpg',	'Дмитрий Сергеевич Иванов',	1978,	8,	'Энергичный и общительный командный игрок, мастерски координирующий проекты и способствующий сотрудничеству в коллективе.'),
+(5,	'5.jpg',	'Алексей Павлович Морозов',	1980,	3,	'Внимательный к деталям новатор с любовью к современным архитектурным эстетикам и преданностью удовлетворению клиентов.'),
+(7,	'7.jpg',	'Николай Евгеньевич Волков',	1975,	2,	'Надежный и аналитический мыслитель, известный своей экспертизой в обеспечении структурной целостности каждого проекта.'),
+(8,	'8.jpg',	'Олег Анатольевич Смирнов',	1965,	6,	'Прагматичный и организованный лидер, который обеспечивает бесперебойную работу фирмы своим эффективным стилем управления.'),
+(9,	'1773206175_val.jpg',	'Стажер',	1988,	2,	'ф');
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `types`
---
-
+DROP TABLE IF EXISTS `job_title`;
 CREATE TABLE `job_title` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `name` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Дамп данных таблицы `types`
---
-
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 INSERT INTO `job_title` (`id`, `name`) VALUES
-(1, 'Архитектор'),
-(2, 'Инженер'),
-(3, 'Дизайнер'),
-(4, 'Генеральный деректор'),
-(5, 'Старший архитектор'),
-(6, 'Директор по операциям'),
-(7, 'Председатель совета директоров'),
-(8, 'Менеджер проектов');
+(1,	'Архитектор'),
+(2,	'Инженер'),
+(3,	'Дизайнер'),
+(4,	'Генеральный деректор'),
+(5,	'Старший архитектор'),
+(6,	'Директор по операциям'),
+(7,	'Председатель совета директоров'),
+(8,	'Менеджер проектов');
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `users`
---
-
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `id` int(11) UNSIGNED NOT NULL,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
   `FIO` varchar(255) DEFAULT NULL,
   `date` varchar(10) DEFAULT NULL,
   `address` varchar(255) DEFAULT NULL,
@@ -93,51 +57,13 @@ CREATE TABLE `users` (
   `blood` varchar(3) DEFAULT NULL,
   `resus` varchar(255) NOT NULL,
   `email` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Дамп данных таблицы `users`
---
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 INSERT INTO `users` (`id`, `FIO`, `date`, `address`, `sex`, `interes`, `vk`, `blood`, `resus`, `email`, `password`) VALUES
-(5, 'Иванов Иван Иванович', '2001-03-02', 'Москва', 'Ж', 'бюдлоирмпавкыувкнпо', 'https://vk.com/feed', 'IV', '-', 'email4@gmail.com', '$2y$10$81RTRmlaIPpji48Y5H1T7evzpF/v0CN4YFUSgigkQGzZTi2qexJTG'),
-(6, 'Иванов Иван Иванович', '2001-03-02', 'Москва', 'М', 'sdfghjkl;', 'https://vk.com/feed', 'I', '+', 'email11@gmail.com', '$2y$10$olY28NnXeXyae314jY3ese.EhQBaI1OQG0PGxzOl1.3ki91YbxlUy');
+(6,	'Иванов Иван Иванович',	'2001-03-02',	'Москва',	'М',	'sdfghjkl;',	'https://vk.com/feed',	'I',	'+',	'email11@gmail.com',	'$2y$10$olY28NnXeXyae314jY3ese.EhQBaI1OQG0PGxzOl1.3ki91YbxlUy'),
+(7,	'Попов Дмитрий Александрович',	'31/07/2005',	'Волгоград, ...',	'М',	'-',	'https://vk.com/axawys',	'I',	'+',	'axawys@gmail.com',	'$2y$10$abcOclz.22MohHsozeS50OKY9l/3fUDU4Wu3JEoFzA6V.g3VZQ6Nq');
 
---
--- Индексы сохранённых таблиц
---
-
---
--- Индексы таблицы `types`
---
-ALTER TABLE `job_title`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `types`
---
-ALTER TABLE `job_title`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT для таблицы `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- 2026-03-11 05:19:23 UTC
